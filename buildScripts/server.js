ef4d1fad711ceee511eb2c6cb311182508014606
psingh7 <praveenkumar_singh@intuit.com>
2016-11-29 02:34:21 +0530

    test new change

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -10,7 +10,6 @@ app.get("/", function(req, res){
     res.sendFile(path.join(__dirname,"../src/index.html" ));
 });
 
-
 app.listen(port, function(err){
     console.log("listining....")
     if(err){
